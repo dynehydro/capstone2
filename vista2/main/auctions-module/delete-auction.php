0337c015b9ad68fa91fcdd1355477fd7118d86cd
Argian Nichole R. Cortez <98294674+dynehydro@users.noreply.github.com>
2024-11-17 23:39:28 +0800

    Delete vista2 directory

--- vista2/main/auctions-module/delete-auction.php
+++
@@ -1,8 +0,0 @@
-    <form method="POST" action="processes/process.art.php?action=delete">
-        <div id="form-block-half">
-            <input type="hidden" id="gallery_id" class="input" name="gallery_id" value="<?php echo $id;?>">
-        </div>
-        <div id="button-block">
-            <input type="submit" value="Delete" onclick="return confirm('Are you sure you want to delete this art piece?');">
-        </div>
-  </form>
--- vista2/main/gallery-module/delete-gallery.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <form method="POST" action="processes/processes.gallery.php?action=delete">
-            <input type="hidden" id="gallery_id" class="input" name="gallery_id" value="<?php echo $id;?>">
-
-            <input type="submit" value="Delete" onclick="return confirm('Are you sure you want to delete this gallery?');">
-  </form>
-</div>
